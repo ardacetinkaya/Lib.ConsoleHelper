b38d38dd7d318b1ad26da0c2ed6ceaae6d7a643f
Arda Cetinkaya <ardacetinkaya@gmail.com>
2019-09-19 14:51:09 +0300

    Create MenuItem.cs

---
+++ src/MenuItem.cs
@@ -0,0 +1,9 @@
+namespace Lib.ConsoleHelper
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Help { get; set; }
+        public string Value { get; set; }
+    }
+}
