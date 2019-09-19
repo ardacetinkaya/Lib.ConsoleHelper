04eacb6ce9efe52d103500856e0b12267506fd02
Arda Cetinkaya <ardacetinkaya@gmail.com>
2019-09-19 14:51:59 +0300

    Create Settings.cs

---
+++ src/Settings.cs
@@ -0,0 +1,11 @@
+namespace Lib.ConsoleHelper
+{
+    using System;
+
+    public class Settings
+    {
+        public string Selection { get; set; }
+        public ConsoleColor SelectionColor { get; set; }
+        public string IntroText { get; internal set; }
+    }
+}
